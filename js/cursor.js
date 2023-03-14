6d29530b8dffb46a0f7284c79b94fdf5c480aad6
Nal <2499908471@qq.com>
2023-03-14 16:56:43 +0800

    Site updated: 2023-03-14 16:56:43

--- js/cursor.js
+++ js/cursor.js
@@ -81,3 +81,11 @@ class Cursor {
     CURSOR = new Cursor();
     // 需要重新获取列表时，使用 CURSOR.refresh()
 })();
+
+// 防抖全局计时器
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
